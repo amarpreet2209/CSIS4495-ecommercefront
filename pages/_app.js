b08f0acd100ca5efbeb1be6cc89fbdd5c0aa7140
Amarpreet Singh <singha163@student.douglascollege.ca>
2023-06-18 12:41:13 -0700

    Basic Navbar with styling created

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,13 @@ import {createGlobalStyle} from "styled-components";
 
 
 const GlobalStyles = createGlobalStyle`
+
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap');
+  
   body {
     padding: 0;
     margin: 0;
+    font-family: 'Roboto', sans-serif;
   }
 `;
 
