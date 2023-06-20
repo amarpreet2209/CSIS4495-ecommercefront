869b8a1182a0b2678a6c2a26f2103cb84f989917
Amarpreet Singh <singha163@student.douglascollege.ca>
2023-06-19 22:50:26 -0700

    Cart icon added

--- components/Button.js
+++ components/Button.js
@@ -5,7 +5,13 @@ const StyledButton = styled.button`
   padding: 5px 15px;
   border-radius: 5px;
   cursor: pointer;
-
+  display: inline-flex;
+  svg {
+    height: 16px;
+    margin-right: 5px;
+  }
+  
+  
   ${props => props.white && !props.outline && css`
     background-color: #fff;
     color: #000;
@@ -27,6 +33,11 @@ const StyledButton = styled.button`
   ${props => props.size === 'l' && css`
     font-size: 1.2rem;
     padding: 10px 20px;
+
+    svg {
+      height: 20px;
+
+    }
   `}
 
 `;
