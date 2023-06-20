3495e00af04ba2feffe16ac10a6637466744b427
Amarpreet Singh <singha163@student.douglascollege.ca>
2023-06-19 23:23:14 -0700

    Database connected and Featured Product being fetched

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 import Header from "../components/Header";
 import Featured from "../components/Featured";
+import {Product} from "../models/Product";
+import {mongooseConnect} from "../lib/mongoose";
 
 
 export default function Home() {
@@ -10,4 +12,14 @@ export default function Home() {
         <Featured/>
       </div>
   )
+}
+
+export async function getServerSideProps() {
+    const featuredProductId = '6483b0b5e8298969cd29ba55';
+    await mongooseConnect();
+    const product = await Product.findById(featuredProductId);
+    
+    return {
+        props: {product: JSON.parse(JSON.stringify(product))}
+    };
 }
