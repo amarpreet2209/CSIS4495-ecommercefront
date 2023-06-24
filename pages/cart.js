88faf2a100863d42dd18c4e35ac1142686868eec
Amarpreet Singh <singha163@student.douglascollege.ca>
2023-06-23 22:45:11 -0700

    Total showing of all products

--- pages/cart.js
+++ pages/cart.js
@@ -64,6 +64,12 @@ export default function CartPage() {
         removeProduct(id);
     }
     
+    let total = 0;
+    for (const productId of cartProducts) {
+        const price = products.find(p => p._id === productId)?.price || 0;
+        total += price;
+    }
+    
     return (
         <>
             <Header></Header>
@@ -113,6 +119,11 @@ export default function CartPage() {
                                                 .length * product.price}</td>
                                         </tr>
                                     ))}
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td>${total}</td>
+                                </tr>
                                 </tbody>
                             </Table>
                         )}
