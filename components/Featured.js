7733369f96b679a891bb0ec12877dc256269fc47
Amarpreet Singh <singha163@student.douglascollege.ca>
2023-06-23 18:16:45 -0700

    Add to cart on New Arrivals updates CartContext now

--- components/Featured.js
+++ components/Featured.js
@@ -46,9 +46,9 @@ const Column = styled.div`
 
 
 export default function Featured({product}) {
-    const {setCartProducts} = useContext(CartContext);
+    const {addProduct} = useContext(CartContext);
    function addFeaturedToCart() {
-       setCartProducts(prev => [...prev, product._id])
+       addProduct(product._id);
    }
     return (
         <Bg>
--- components/ProductBox.js
+++ components/ProductBox.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import Button from "./Button";
 import Link from "next/link";
+import {useContext} from "react";
+import {CartContext} from "./CartContext";
 
 
 const ProductWrapper = styled.div`
@@ -46,6 +48,7 @@ const Price = styled.div`
 `;
 
 export default function ProductBox({_id, title, description, price, images}) {
+    const {addProduct} = useContext(CartContext);
     const url = '/product/' + _id;
     return (
         <ProductWrapper>
@@ -60,7 +63,7 @@ export default function ProductBox({_id, title, description, price, images}) {
                     <Price>
                         ${price}
                     </Price>
-                    <Button primary outline>Add to cart</Button>
+                    <Button primary outline onClick={() => addProduct(_id)}>Add to cart</Button>
                 </PriceRow>
             </ProductInfoBox>
         </ProductWrapper>
