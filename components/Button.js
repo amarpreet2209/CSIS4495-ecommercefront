63a06100f0eb21c96e31c9b2f5c6d07757be59f3
Amarpreet Singh <singha163@student.douglascollege.ca>
2023-06-23 17:33:18 -0700

    Poppins font, button color changed to green

--- components/Button.js
+++ components/Button.js
@@ -9,11 +9,11 @@ export const ButtonStyle = css`
   display: inline-flex;
   align-items: center;
   text-decoration: none;
+  font-weight: 500;
   svg {
     height: 16px;
     margin-right: 5px;
   }
-
 
   ${props => props.white && !props.outline && css`
     background-color: #fff;
--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,13 @@ import {createGlobalStyle} from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
 
-  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap');
   
   body {
     background-color: #eee;
     padding: 0;
     margin: 0;
-    font-family: 'Roboto', sans-serif;
+    font-family: 'Poppins', sans-serif;
   }
 `;
 
