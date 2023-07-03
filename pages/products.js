17a78e562b99442d8f285065aec66fa090ae9c30
Amarpreet Singh <singha163@student.douglascollege.ca>
2023-07-02 18:29:05 -0700

    All products showing now

--- pages/products.js
+++ pages/products.js
@@ -1,15 +1,33 @@
 import Header from "../components/Header";
 import styled from "styled-components";
+import {mongooseConnect} from "../lib/mongoose";
+import {Product} from "../models/Product";
+import ProductsGrid from "../components/ProductsGrid";
+import Center from "../components/Center";
 
 const Title = styled.h1`
   font-size: 1.5rem;
 `;
 
-export default function ProductsPage() {
+export default function ProductsPage({products}) {
     return (
         <>
             <Header/>
-            <Title>All Products</Title>
+            <Center>
+                <Title>All Products</Title>
+                <ProductsGrid products={products}/>
+            </Center>
         </>
     );
 }
+
+export async function getServerSideProps() {
+    await mongooseConnect();
+    const products =  await Product.find({}, null, {sort: {'_id': -1}})
+    return {
+        props: {
+            products: JSON.parse(JSON.stringify(products))
+        }
+    }
+}
+
