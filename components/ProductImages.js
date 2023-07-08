b3c27f0fc02eacff11189c59a248361480ac5ddf
Amarpreet Singh <singha163@student.douglascollege.ca>
2023-07-07 23:57:41 -0700

    BigImage size fixed now

--- components/ProductImages.js
+++ components/ProductImages.js
@@ -1,20 +1,19 @@
 import styled from "styled-components";
+import {useState} from "react";
 
-export default function ProductImages({images}) {
-    
-    const Image = styled.img`
+const Image = styled.img`
         max-width: 100%;
         max-height: 100%;
     `;
-    
-    const ImageButtons = styled.div`
+
+const ImageButtons = styled.div`
       display: flex;
       gap: 10px;
       flex-grow: 0;
       margin-top: 10px;
     `;
-    
-    const ImageButton = styled.div`
+
+const ImageButton = styled.div`
       border: 2px solid #ccc;
       height:  40px;
       padding: 2px;
@@ -22,13 +21,23 @@ export default function ProductImages({images}) {
       border-radius: 5px;
       //mix-blend-mode: inherit;
     `;
+
+const BigImage = styled.img`
+        max-width: 100%;
+        max-height: 200px;
+`;
+
+
+export default function ProductImages({images}) {
+
+    const [activeImage, setActiveImage] = useState(images?.[0]);
     
     return (
         <>
-            <Image src={images?.[0]} alt={""}/>
+            <BigImage src={activeImage} alt={""}/>
             <ImageButtons>
                 {images.map(image => (
-                    <ImageButton>
+                    <ImageButton onClick={() => setActiveImage(image)}>
                         <Image src={image} alt={""} />
                     </ImageButton>
                 ))}
