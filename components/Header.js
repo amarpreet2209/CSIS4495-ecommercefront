9ec90fc3ce2d4606d50265944c8df386cd2c13a3
Amarpreet Singh <singha163@student.douglascollege.ca>
2023-07-08 15:48:58 -0700

    Navbar button works

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,9 @@
 import Link from "next/link";
 import styled from "styled-components";
 import Center from "./Center";
-import {useContext} from "react";
+import {useContext, useState} from "react";
 import {CartContext} from "./CartContext";
+import BarsIcon from "./icons/Bars";
 
 const StyledHeader = styled.header`
     background-color: #222
@@ -20,30 +21,64 @@ const Wrapper = styled.div`
 `;
 
 const StyledNav = styled.nav`
-  display: flex;
-  gap: 10px;
+  ${props => props.mobileNavActive ? `
+    display: block;
+  `:`
+    display: none;
+  `}
+  gap: 15px;
+  position: fixed;
+  top: 50px;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  padding: 20px;
+  background-color: #222;
+  @media screen and (min-width: 768px) {
+    display: flex;
+    position: static;
+    padding: 0;
+  }
 `;
 
 
 const NavLink = styled(Link)`
+  display: block;
   color: #aaa;
   text-decoration: none;
 `;
 
+const NavButton = styled.button`
+  background-color: transparent;
+  width: 30px;
+  height: 30px;
+  border: 0;
+  color: white;
+  cursor: pointer;
+
+  @media screen and (min-width: 768px) {
+    display: none;
+  }
+`;
+
 export default function Header() {
     const {cartProducts} = useContext(CartContext);
+    const [mobileNavActive, setMobileNavActive] = useState(false);
     return (
         <StyledHeader>
             <Center>
                 <Wrapper>
                     <Logo href={'/'}>Ecommerce</Logo>
-                    <StyledNav>
+                    <StyledNav mobileNavActive={mobileNavActive}>
                         <NavLink href={'/'}>Home</NavLink>
                         <NavLink href={'/products'}>All Products</NavLink>
                         <NavLink href={'/categories'}>Categories</NavLink>
                         <NavLink href={'/account'}>Account</NavLink>
                         <NavLink href={'/cart'}>Cart ({cartProducts.length})</NavLink>
                     </StyledNav>
+                    <NavButton onClick={() => setMobileNavActive(prev => !prev)}>
+                        <BarsIcon/>
+                    </NavButton>
                 </Wrapper>
 
             </Center>
