8db4313fd406639cc6f3ca20e393a09b9b6b44cf
Amarpreet Singh <singha163@student.douglascollege.ca>
2023-07-07 20:33:11 -0700

    Product Page showing Info and First image

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -3,13 +3,33 @@ import Center from "../../components/Center";
 import Title from "../../components/Title";
 import {mongooseConnect} from "../../lib/mongoose";
 import {Product} from "../../models/Product";
+import styled from "styled-components";
+import WhiteBox from "../../components/WhiteBox";
+
+const ColWrapper = styled.div`
+    display: grid;
+    grid-template-columns: .8fr 1.2fr;
+    gap: 40px;
+    margin-top: 40px;
+`;
+
 
 export default function ProductPage({product}) {
     return (
         <>
             <Header/>
             <Center>
-                <Title>{product.title}</Title>
+                <ColWrapper>
+                    <WhiteBox>
+                        <img style={{maxWidth: '100%'}} src={product.images?.[0]} alt={""}/>
+                    </WhiteBox>
+                    <div>
+                        <Title>{product.title}</Title>
+                        <p>{product.description}</p>
+                    </div>
+                    
+                </ColWrapper>
+               
             </Center>
         </>
     )
