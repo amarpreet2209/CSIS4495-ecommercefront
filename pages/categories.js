b82261bbc88cd023f39c63784b0f592eb90a6592
Amarpreet Singh <singha163@student.douglascollege.ca>
2023-07-09 16:24:31 -0700

    CategoriesPage added

---
+++ pages/categories.js
@@ -0,0 +1,11 @@
+import Title from "../components/Title";
+import Header from "../components/Header";
+
+export default function CategoriesPage() {
+    return (
+        <>
+            <Header/>
+            <Title>All Categories</Title>
+        </>
+    )
+}
