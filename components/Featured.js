44634e6e48759cfb6e7f2e5750a4164f6fb125f7
Amarpreet Singh <singha163@student.douglascollege.ca>
2023-07-15 16:33:05 -0700

    Animation issue fixed

--- components/Featured.js
+++ components/Featured.js
@@ -1,39 +1,33 @@
-import Center from "./Center";
 import styled from "styled-components";
-import Button from "./Button";
-import ButtonLink from "./ButtonLink";
-import CartIcon from "./icons/Cart";
-import {useContext} from "react";
-import {CartContext} from "./CartContext";
+
+import {RevealWrapper} from 'next-reveal'
 import FlyingButton from "./FlyingButton";
-import {RevealWrapper} from "next-reveal";
+import CartIcon from "./icons/Cart";
+import ButtonLink from "./ButtonLink";
+import Center from "./Center";
 
 const Bg = styled.div`
   background-color: #222;
-  color: #fff;
+  color:#fff;
   padding: 50px 0;
-`
-
+`;
 const Title = styled.h1`
   margin:0;
-  font-weight: normal;
-  font-size: 1.5rem;
-
+  font-weight:normal;
+  font-size:1.5rem;
   @media screen and (min-width: 768px) {
-    font-size: 3rem;
+    font-size:3rem;
   }
 `;
-
 const Desc = styled.p`
-  color: #aaa;
-  font-size: 0.8rem;
+  color:#aaa;
+  font-size:.8rem;
 `;
-
 const ColumnsWrapper = styled.div`
   display: grid;
   grid-template-columns: 1fr;
   gap: 40px;
-  img {
+  img.main{
     max-width: 100%;
     max-height: 200px;
     display: block;
@@ -41,62 +35,76 @@ const ColumnsWrapper = styled.div`
   }
   div:nth-child(1) {
     order: 2;
+    margin-left: auto;
+    margin-right: auto;
   }
- 
   @media screen and (min-width: 768px) {
     grid-template-columns: 1.1fr 0.9fr;
-    div:nth-child(1) {
+    & > div:nth-child(1) {
       order: 0;
     }
-    img {
+    img{
       max-width: 100%;
     }
   }
 `;
-
-const ButtonsWrapper = styled.div`
-  display: flex;
-  gap: 10px;
-  margin-top: 25px;
-`;
-
 const Column = styled.div`
   display: flex;
   align-items: center;
-  
+`;
+const ButtonsWrapper = styled.div`
+  display: flex;
+  gap:10px;
+  margin-top:25px;
+`;
+const CenterImg = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
 `;
 
+const ImgColumn = styled(Column)`
+  & > div{
+    width: 100%;
+  }
+`;
+
+const ContentWrapper = styled.div`
+`;
 
 export default function Featured({product}) {
-
     return (
         <Bg>
             <Center>
                 <ColumnsWrapper>
                     <Column>
                         <div>
-                            
                             <RevealWrapper origin={'left'} delay={0}>
-                                <Title>{product.title}</Title>
-                                <Desc>{product.description}</Desc>
-                                <ButtonsWrapper>
-                                    <ButtonLink href={'/product/' + product._id} outline={1} white={1}>Read more</ButtonLink>
-                                    <FlyingButton white _id={product._id} src={product.images?.[0]}>
-                                        <CartIcon/>
-                                        Add to cart
-                                    </FlyingButton>
-                                </ButtonsWrapper>
+                                <ContentWrapper>
+                                    <Title>{product.title}</Title>
+                                    <Desc>{product.description}</Desc>
+                                    <ButtonsWrapper>
+                                        <ButtonLink href={'/product/'+product._id} outline={1} white={1}>Read more</ButtonLink>
+                                        <FlyingButton white={1} _id={product._id} src={product.images?.[0]}>
+                                            <CartIcon />
+                                            Add to cart
+                                        </FlyingButton>
+                                    </ButtonsWrapper>
+                                </ContentWrapper>
                             </RevealWrapper>
                         </div>
                     </Column>
-                    <Column>
+                    <ImgColumn>
                         <RevealWrapper delay={0}>
-                            <img src='https://149426355.v2.pressablecdn.com/wp-content/uploads/2021/10/mbp-2021-bbedit-lede.png'  alt=""/>
+                            <CenterImg>
+                                <img className={'main'} src={product.images?.[0]} alt=""/>
+                            </CenterImg>
                         </RevealWrapper>
-                       
-                    </Column>
+                    </ImgColumn>
                 </ColumnsWrapper>
             </Center>
+        
         </Bg>
-    )
+    );
 }
--- components/FlyingButton.js
+++ components/FlyingButton.js
@@ -1,64 +1,79 @@
 import styled from "styled-components";
-import {ButtonStyle} from "./Button";
-import {primary} from "../lib/colors";
-import FlyingButtonOriginal from "react-flying-item"
 import {useContext, useEffect, useRef, useState} from "react";
 import {CartContext} from "./CartContext";
+import {ButtonStyle} from "./Button";
+import {primary} from "../lib/colors";
 
-const FlyingButtonWrapper= styled.div`
-  button {
+const FlyingButtonWrapper = styled.div`
+  button{
     ${ButtonStyle};
     ${props => props.main ? `
-        background-color: ${primary};
-        color:white;
-    `:`
-        background-color: transparent;
-        border: 1px solid ${primary};
-        color: ${primary};
-    `};
-    
+      background-color: ${primary};
+      color:white;
+    ` : `
+      background-color: transparent;
+      border: 1px solid ${primary};
+      color:${primary};
+    `}
     ${props => props.white && `
-        background-color: white;
-        border: 1px solid white;
-    `};
+      background-color: white;
+      border: 1px solid white;
+      font-weight:500;
+    `}
+  }
+  @keyframes fly{
+    100%{
+      top:0;
+      left:65%;
+      opacity: 0;
+      display:none;
+      max-width: 50px;
+      max-height: 50px;
+    }
+  }
+  img{
+    display:none;
+    max-width: 100px;
+    max-height: 100px;
+    opacity: 1;
+    position: fixed;
+    z-index: 5;
+    animation: fly 1s;
+    border-radius: 10px;
   }
 `;
 
 export default function FlyingButton(props) {
     const {addProduct} = useContext(CartContext);
-    const [hovers, setHovers] = useState(0);
-    const wrapperRef = useRef();
-    const pixelRef = useRef();
-    const [show, setShow] = useState(false);
-    
-    useEffect(() => {})
-    
+    const imgRef = useRef();
+    function sendImageToCart(ev) {
+        imgRef.current.style.display = 'inline-block';
+        imgRef.current.style.left = (ev.clientX-50) + 'px';
+        imgRef.current.style.top = (ev.clientY-50) + 'px';
+        setTimeout(() => {
+            imgRef.current.style.display = 'none';
+        }, 1000);
+    }
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const reveal = imgRef.current?.closest('div[data-sr-id]');
+            if (reveal?.style.opacity === '1') {
+                // visible
+                reveal.style.transform = 'none';
+            }
+        }, 100);
+        
+        return () => clearInterval(interval);
+    }, []);
     return (
         <>
-            <div ref={pixelRef} style={{width:'1px', height:'1px'}}></div>
-            {show && (
-                <FlyingButtonWrapper
-                    onMouseEnter={prev => setHovers(prev+1)}
-                    main={props.main}
-                    white={props.white}
-                    onClick={() => addProduct(props._id)}>
-                    <FlyingButtonOriginal
-                        {...props}
-                        targetTop={'5%'}
-                        targetLeft={'95%'}
-                        flyingItemStyling={{
-                            width: 'auto',
-                            height: 'auto',
-                            maxWidth: '60px',
-                            maxHeight: '60px',
-                            borderRadius: 0
-                        }}>
-                    
-                    </FlyingButtonOriginal>
-                </FlyingButtonWrapper>
-            )}
+            <FlyingButtonWrapper
+                white={props.white}
+                main={props.main}
+                onClick={() => addProduct(props._id)}>
+                <img src={props.src} alt="" ref={imgRef} />
+                <button onClick={ev => sendImageToCart(ev)} {...props} />
+            </FlyingButtonWrapper>
         </>
-        
-    )
-   
+    );
 }
