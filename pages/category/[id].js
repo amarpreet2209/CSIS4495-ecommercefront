9c89ed9d6435f3d143ef1183ba3835ce6e6c3b99
Amarpreet Singh <singha163@student.douglascollege.ca>
2023-07-14 17:04:19 -0700

    Filters formatting improved

--- pages/category/[id].js
+++ pages/category/[id].js
@@ -4,23 +4,55 @@ import {Category} from "../../models/Category";
 import {Product} from "../../models/Product";
 import ProductsGrid from "../../components/ProductsGrid";
 import Title from "../../components/Title";
+import styled from "styled-components";
+
+const CategoryHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  
+  h1 {
+    font-size: 1.5em
+  }
+`;
+
+const FiltersWrapper = styled.div`
+  display: flex;
+  gap: 15px;
+`;
+
+const Filter = styled.div`
+  background-color: #ddd;
+  padding: 5px 10px;
+  border-radius: 5px;
+
+  display: flex;
+  gap: 5px;
+`;
+
 
 export default function CategoryPage({category, products}) {
     return (
         <>
             <Header/>
             <Center>
-                <Title>{category.name}</Title>
-                {category.properties.map(prop => (
-                    <div>
-                        {prop.name}
-                        <select>
-                            {prop.values.map(val => (
-                                <option value={val}>{val}</option>
-                            ))}
-                        </select>
-                    </div>
-                ))}
+                <CategoryHeader>
+                    <h1>{category.name}</h1>
+                    <FiltersWrapper>
+                        {category.properties.map(prop => (
+                            <Filter>
+                                <span>{prop.name}</span>
+                                <select>
+                                    {prop.values.map(val => (
+                                        <option value={val}>{val}</option>
+                                    ))}
+                                </select>
+                            </Filter>
+                        ))}
+                    </FiltersWrapper>
+                    
+                </CategoryHeader>
+                
                 
                 <ProductsGrid products={products}/>
             </Center>
