14b41c3a17584a1c5ad03c1fcef38fe5b6ce7b49
Amarpreet Singh <singha163@student.douglascollege.ca>
2023-07-15 21:13:16 -0700

    Formatting Add to cart button

--- components/ProductBox.js
+++ components/ProductBox.js
@@ -7,7 +7,11 @@ import {primary} from "../lib/colors";
 import FlyingButton from "./FlyingButton";
 
 const ProductWrapper = styled.div`
-
+  button {
+    width: 100%;
+    text-align: center;
+    justify-content: center;
+  }
 `;
 const WhiteBox = styled(Link)`
   background-color: #fff;
--- pages/search.js
+++ pages/search.js
@@ -6,23 +6,33 @@ import {useCallback, useEffect, useState} from "react";
 import axios from "axios";
 import ProductsGrid from "../components/ProductsGrid";
 import {debounce} from "lodash";
+import Spinner from "../components/Spinner";
 
 const SearchInput = styled(Input)`
   padding: 5px 10px;
   border-radius: 5px;
-  margin: 30px 0 30px;
   font-size: 1.4rem;
 `;
 
-export default function SearchPage() {
+const InputWrapper = styled.div`
+  position: sticky;
+  top: 68px;
+  margin: 25px 0;
+  padding: 5px 0;
+  background-color: #eeeeeeaa;
+`;
+
+export default function SearchPage( ) {
     const [phrase, setPhrase] = useState('');
     const [products, setProducts] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    
     const debouncedSearch = useCallback(
-                        debounce(phrase => searchProducts(phrase)
-                            ,500), []);
+                        debounce(searchProducts,500), []);
     
     useEffect(() => {
         if (phrase.length > 0) {
+            setIsLoading(true);
             debouncedSearch(phrase);
         } else {
             setProducts([]);
@@ -34,18 +44,33 @@ export default function SearchPage() {
         axios.get('/api/products?phrase=' + encodeURIComponent(phrase))
             .then((response) => {
                 setProducts(response.data);
+                setIsLoading(false);
             });
     }
     return (
         <>
             <Header/>
             <Center>
+            <InputWrapper>
                 <SearchInput
                     autoFocus
                     value={phrase}
                     onChange={ev => setPhrase(ev.target.value)}
                     placeholder={"Search for products..."}/>
-                <ProductsGrid products={products}/>
+                
+                {!isLoading && phrase !== "" && products.length === 0 && (
+                    <h2>No products found for query "{phrase}"</h2>
+                )}
+                
+                {isLoading && (
+                    <Spinner fullWidth={true}/>
+                )}
+                
+                {!isLoading && products.length > 0 && (
+                    <ProductsGrid products={products}/>
+                )}
+            </InputWrapper>
+            
             </Center>
         </>
     );
