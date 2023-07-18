db02df5fe6fd6f2aad22aa1ac015b313db1019f3
Amarpreet Singh <singha163@student.douglascollege.ca>
2023-07-18 14:54:57 -0700

    Account Page added

---
+++ pages/account.js
@@ -0,0 +1,14 @@
+import Header from "../components/Header";
+import Center from "../components/Center";
+import Title from "../components/Title";
+
+export default function AccountPage() {
+    return (
+        <>
+            <Header/>
+            <Center>
+                <Title>Account</Title>
+            </Center>
+        </>
+    )
+}
