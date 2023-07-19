ffaac1532794a22d42612f6046f1ccc88cbde965
Amarpreet Singh <singha163@student.douglascollege.ca>
2023-07-19 10:01:45 -0700

    Google signin works now, port changed to 4000. New gcp project setup

--- pages/account.js
+++ pages/account.js
@@ -1,15 +1,30 @@
 import Header from "../components/Header";
 import Center from "../components/Center";
 import Title from "../components/Title";
-import {useSession} from "next-auth/react";
+import {signIn, signOut, useSession} from "next-auth/react";
+import Button from "../components/Button";
 
 export default function AccountPage() {
-    const session = useSession();
+    const {data: session} = useSession();
+    async function logout() {
+        await signOut({
+            callback: process.env.NEXT_PUBLIC_URL
+        });
+    }
+    async function login() {
+        await signIn('google');
+    }
     return (
         <>
             <Header/>
             <Center>
                 <Title>Account</Title>
+                {session && (
+                    <Button primary onClick={logout}>Logout</Button>
+                )}
+                {!session && (
+                    <Button primary onClick={login}>Login</Button>
+                )}
             </Center>
         </>
     )
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,14 +1,14 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 import {MongoDBAdapter} from "@next-auth/mongodb-adapter";
-import clientPromise from "@/lib/mongodb";
+import clientPromise from "../../../lib/mongodb";
 
 export const authOptions = {
     providers: [
 
         GoogleProvider({
-            clientId: process.env.GOOGLE_ID,
-            clientSecret: process.env.GOOGLE_SECRET
+            clientId: process.env.GOOGLE_FRONT_ID,
+            clientSecret: process.env.GOOGLE_FRONT_SECRET
         })
     ],
     adapter: MongoDBAdapter(clientPromise)
