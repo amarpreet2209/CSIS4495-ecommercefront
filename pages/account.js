7b38a2add30f69e5d134fd7f30f6a22dc6352dab
Amarpreet Singh <singha163@student.douglascollege.ca>
2023-07-19 11:36:13 -0700

    Existing address getting loaded on account information form

--- pages/account.js
+++ pages/account.js
@@ -7,8 +7,9 @@ import styled from "styled-components";
 import WhiteBox from "../components/WhiteBox";
 import {RevealWrapper} from "next-reveal";
 import Input from "../components/Input";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import axios from "axios";
+import Spinner from "../components/Spinner";
 
 const ColsWrapper = styled.div`
     display: grid;
@@ -28,6 +29,7 @@ export default function AccountPage() {
     const [postalCode, setPostalCode] = useState("");
     const [streetAddress, setStreetAddress] = useState("");
     const [country, setCountry] = useState("");
+    const [loaded, setLoaded] = useState(false);
     
     const CityHolder = styled.div`
         display: flex;
@@ -47,6 +49,18 @@ export default function AccountPage() {
         const data = {name, email, city, streetAddress, postalCode, country};
         axios.put('/api/address', data);
     }
+    
+    useEffect(() => {
+        axios.get('/api/address').then((response) => {
+            setName(response.data?.name);
+            setEmail(response.data?.email);
+            setCity(response.data?.city);
+            setPostalCode(response.data?.postalCode);
+            setStreetAddress(response.data?.streetAddress);
+            setCountry(response.data?.country);
+            setLoaded(true);
+        });
+    }, []);
     return (
         <>
             <Header/>
@@ -60,55 +74,63 @@ export default function AccountPage() {
                     <RevealWrapper delay={100}>
                         <WhiteBox>
                             <h2>Account Details</h2>
-                            <Input
-                                type="text"
-                                placeholder="Name"
-                                name={"name"}
-                                value={name}
-                                onChange={(ev) => setName(ev.target.value)}
-                            />
-                            <Input
-                                type="text"
-                                placeholder="Email"
-                                name={"email"}
-                                value={email}
-                                onChange={(ev) => setEmail(ev.target.value)}
-                            />
-                            
-                            <CityHolder>
-                                <Input
-                                    type="text"
-                                    placeholder="City"
-                                    name={"city"}
-                                    value={city}
-                                    onChange={(ev) => setCity(ev.target.value)}
-                                />
-                                <Input
-                                    type="text"
-                                    placeholder="Postal Code"
-                                    name={"postalCode"}
-                                    value={postalCode}
-                                    onChange={(ev) => setPostalCode(ev.target.value)}
-                                />
-                            </CityHolder>
-                            
-                            <Input
-                                type="text"
-                                placeholder="Street Address"
-                                name={"streetAddress"}
-                                value={streetAddress}
-                                onChange={(ev) => setStreetAddress(ev.target.value)}
-                            />
-                            <Input
-                                type="text"
-                                placeholder="Country"
-                                name={"country"}
-                                value={country}
-                                onChange={(ev) => setCountry(ev.target.value)}
-                            />
-                           
-                            <Button black block onClick={saveAddress}>Save</Button>
-                            <hr/>
+                            {!loaded && (
+                                <Spinner fullWidth={true}/>
+                            )}
+                            {loaded && (
+                                <>
+                                    <Input
+                                        type="text"
+                                        placeholder="Name"
+                                        name={"name"}
+                                        value={name}
+                                        onChange={(ev) => setName(ev.target.value)}
+                                    />
+                                    <Input
+                                        type="text"
+                                        placeholder="Email"
+                                        name={"email"}
+                                        value={email}
+                                        onChange={(ev) => setEmail(ev.target.value)}
+                                    />
+                                    
+                                    <CityHolder>
+                                        <Input
+                                            type="text"
+                                            placeholder="City"
+                                            name={"city"}
+                                            value={city}
+                                            onChange={(ev) => setCity(ev.target.value)}
+                                        />
+                                        <Input
+                                            type="text"
+                                            placeholder="Postal Code"
+                                            name={"postalCode"}
+                                            value={postalCode}
+                                            onChange={(ev) => setPostalCode(ev.target.value)}
+                                        />
+                                    </CityHolder>
+                                    
+                                    <Input
+                                        type="text"
+                                        placeholder="Street Address"
+                                        name={"streetAddress"}
+                                        value={streetAddress}
+                                        onChange={(ev) => setStreetAddress(ev.target.value)}
+                                    />
+                                    <Input
+                                        type="text"
+                                        placeholder="Country"
+                                        name={"country"}
+                                        value={country}
+                                        onChange={(ev) => setCountry(ev.target.value)}
+                                    />
+                                    
+                                    <Button black block onClick={saveAddress}>Save</Button>
+                                    <hr/>
+                                    
+                                </>
+                            )}
                             {session && (
                                 <Button primary onClick={logout}>Logout</Button>
                             )}
