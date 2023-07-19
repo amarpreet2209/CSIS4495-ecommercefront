5a4114fa337116c5d3f24e2319d8d19afcaacf0a
Amarpreet Singh <singha163@student.douglascollege.ca>
2023-07-19 10:59:59 -0700

    Address model updated

--- models/Address.js
+++ models/Address.js
@@ -1,7 +1,7 @@
 import mongoose, {model, models, Schema} from "mongoose";
 
 const AddressSchema = new Schema({
-    user: mongoose.Types.ObjectId,
+    userEmail: {type: String, unique: true, required: true},
     name: String,
     email: String,
     city: String,
