05d20398cfc3df64ee5e860e3767305636a8d1e2
Amarpreet Singh <singha163@student.douglascollege.ca>
2023-07-20 08:50:52 -0700

    Wishlist button added to UI

--- components/ProductBox.js
+++ components/ProductBox.js
@@ -5,6 +5,7 @@ import {useContext} from "react";
 import {CartContext} from "./CartContext";
 import {primary} from "../lib/colors";
 import FlyingButton from "./FlyingButton";
+import HeartOutlineIcon from "./icons/HeartOutlineIcon";
 
 const ProductWrapper = styled.div`
   button {
@@ -22,6 +23,7 @@ const WhiteBox = styled(Link)`
   align-items: center;
   justify-content: center;
   border-radius: 10px;
+  position: relative;
   img {
     max-width: 100%;
     max-height: 80px;
@@ -62,7 +64,20 @@ const Price = styled.div`
     }
 `;
 
-
+const WishlistButton = styled.button`
+  border:0;
+  width: 40px !important;
+  height: 40px;
+  padding: 10px;
+  position: absolute;
+  top: 0;
+  right: 0;
+  background: transparent;
+  cursor: pointer;
+  svg {
+    width: 16px;
+  }
+`;
 
 export default function ProductBox({_id, title, description, price, images}) {
     
@@ -71,6 +86,9 @@ export default function ProductBox({_id, title, description, price, images}) {
         <ProductWrapper>
             <WhiteBox href={url}>
                 <div>
+                    <WishlistButton>
+                        <HeartOutlineIcon/>
+                    </WishlistButton>
                     <img src={images[0]} alt=""/>
                 </div>
             </WhiteBox>
