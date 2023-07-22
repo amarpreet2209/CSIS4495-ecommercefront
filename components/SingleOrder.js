ad6473f29b7cf9e6e395b5580e212d6b82d3db19
Amarpreet Singh <singha163@student.douglascollege.ca>
2023-07-22 10:22:30 -0700

    Date formatting orders done

--- components/SingleOrder.js
+++ components/SingleOrder.js
@@ -1,12 +1,32 @@
+import styled from "styled-components";
+import {format} from "date-fns-tz";
+
+const StyledOrder = styled.div`
+  margin: 5px 0;
+  padding: 5px 0;
+  border-bottom: 1px solid #ddd;
+`;
+
+const ProductRow = styled.div`
+  span {
+    color: #aaa;
+  }
+`;
+
+function formatDate(date) {
+    const vancouverTimezone = 'America/Vancouver';
+    return format(date, 'MM/dd/yyyy, h:mm:ss a', {timeZone: vancouverTimezone});
+}
 export default function SingleOrder({line_items, createdAt}) {
     return (
-        <div>
-            <time>{createdAt}</time>
+        <StyledOrder>
+            <time>{formatDate(new Date(createdAt))}</time>
             {line_items.map(item => (
-                <div>
+                <ProductRow>
+                    <span>{item.quantity} x </span>
                     {item.price_data.product_data.name}
-                </div>
+                </ProductRow>
             ))}
-        </div>
+        </StyledOrder>
     )
 }
