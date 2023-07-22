00425cc9ce1053b50490c869483ff18da74c28f8
Amarpreet Singh <singha163@student.douglascollege.ca>
2023-07-22 10:27:15 -0700

    Order formatting improved

--- components/SingleOrder.js
+++ components/SingleOrder.js
@@ -5,6 +5,17 @@ const StyledOrder = styled.div`
   margin: 5px 0;
   padding: 5px 0;
   border-bottom: 1px solid #ddd;
+  
+  display: flex;
+  gap: 20px;
+  align-items: center;
+  
+  time {
+    font-size: 1rem;
+    font-weight: bold;
+    color: #777;
+  }
+  
 `;
 
 const ProductRow = styled.div`
@@ -20,13 +31,17 @@ function formatDate(date) {
 export default function SingleOrder({line_items, createdAt}) {
     return (
         <StyledOrder>
-            <time>{formatDate(new Date(createdAt))}</time>
-            {line_items.map(item => (
-                <ProductRow>
-                    <span>{item.quantity} x </span>
-                    {item.price_data.product_data.name}
-                </ProductRow>
-            ))}
+            <div>
+                <time>{formatDate(new Date(createdAt))}</time>
+            </div>
+            <div>
+                {line_items.map(item => (
+                    <ProductRow>
+                        <span>{item.quantity} x </span>
+                        {item.price_data.product_data.name}
+                    </ProductRow>
+                ))}
+            </div>
         </StyledOrder>
     )
 }
